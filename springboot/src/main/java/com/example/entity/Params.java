df7c7f52f24440f68a386a2fcb65b861ebca30af
Phoenix-77777 <3046252080@qq.com>
2024-10-29 09:21:54 +0800

    Add files via upload

---
+++ springboot/src/main/java/com/example/entity/Params.java
@@ -0,0 +1,40 @@
+package com.example.entity;
+
+public class Params {
+        private String name;
+        private String number;
+        private Integer pageNum;
+        private Integer pageSize;
+
+        public String getName() {
+                return name;
+        }
+
+        public void setName(String name) {
+                this.name = name;
+        }
+
+        public String getNumber() {
+                return number;
+        }
+
+        public void setNumber(String number) {
+                this.number = number;
+        }
+
+        public Integer getPageNum() {
+                return pageNum;
+        }
+
+        public void setPageNum(Integer pageNum) {
+                this.pageNum = pageNum;
+        }
+
+        public Integer getPageSize() {
+                return pageSize;
+        }
+
+        public void setPageSize(Integer pageSize) {
+                this.pageSize = pageSize;
+        }
+}
